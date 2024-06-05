6359a38bb4fa7eed1ee0746dd37eac5f6294995a
naberius616 <pansrzpan@gmail.com>
2024-06-05 16:23:02 -0300

    pronto 2

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -65,45 +65,19 @@ const Home = () => {
           </View>
         </View>
       )}
-      {filterValue === 'Turismo' && (
+    {filterValue === 'Turismo' && (
         <View>
-          <Marker
-             coordinate={{ latitude: -23.5092161, longitude: -46.6517039 }}
-             title="Área de turismo"
-             description="Área mais popular da cidade"
-           >
-             <Image source={{uri: "https://cdn.icon-icons.com/icons2/1310/PNG/512/city_86340.png"}} style={{ width: 60, height: 60 }} />
-           </Marker>
-           <Marker
-             coordinate={{ latitude: -23.5092161, longitude: -46.6517039 }}
-             title="Área de turismo"
-             description="Área mais popular da cidade"
-           >
-             <Image source={{uri: "https://greenpng.com/wp-content/uploads/2020/07/20200728_132928.png"}} style={{ width: 200, height: 200 }} />
-          </Marker>
-        </View>     
+          <MapMarker markerTitle="Área de turismo" markerDescription="Área popular para turismo" markerLatitude={-23.5092161} markerLongitude={-46.6517039} markerHeight={60} markerWidth={60} circleWidth={200} circleHeight={200} markerUrl={"https://cdn.icon-icons.com/icons2/1310/PNG/512/city_86340.png"}/>
+        </View>
       )}
 
-  {filterValue === 'Pesca' && (
-        <View>
-        <Marker
-          coordinate={{ latitude: -25.29704654, longitude: -46.7009554 }}
-          title="Área de pesca"
-          description="Animais com perigo de extinção ao redor da área"
-        >
-          <Image source={{uri: "https://cdn-icons-png.flaticon.com/512/4207/4207682.png"}} style={{ width: 60, height: 60 }} />
-        </Marker>
-        <Marker
-          coordinate={{ latitude: -25.29704654, longitude: -46.7009554 }}
-          title="Área de pesca"
-          description="Animais com perigo de extinção ao redor da área"
-        >
-          <Image source={{uri: "https://greenpng.com/wp-content/uploads/2020/07/20200728_132928.png"}} style={{ width: 200, height: 200 }} />
-        </Marker>
+    {filterValue === 'Pesca' && (
+      <View>
+        <MapMarker markerTitle="Área de pesca" markerDescription="Animais com perigo de extinção ao redor da área" markerLatitude={-25.29704654} markerLongitude={-46.7009554} markerHeight={60} markerWidth={60} circleWidth={200} circleHeight={200} markerUrl={"https://cdn-icons-png.flaticon.com/512/4207/4207682.png"}/>
       </View>
-      )}
+  )}
 
-  {filterValue === 'Animais em Extinção' && (
+    {filterValue === 'Animais em Extinção' && (
         <View>
         <Marker
           coordinate={{ latitude: -24.4733834, longitude: -46.2780295 }}
@@ -122,18 +96,11 @@ const Home = () => {
       </View>
       )}
 
-{filterValue === 'Região Perigosa' && (
-  <View>
-  <Marker
-    coordinate={{ latitude: -23.9888838, longitude: -46.5276974 }}
-    title="Região Perigosa"
-    description="Animais selvagens ao redor dessa área"
-  >
-    <Image source={{uri: "https://cdn-icons-png.freepik.com/512/4201/4201973.png"}} style={{ width: 80, height: 80 }} />
-  </Marker>
-</View>
-)}
-
+    {filterValue === 'Região Perigosa' && (
+      <View>
+        <MapMarker markerTitle="Perigo de Extinção" markerDescription="Animais com perigo de extinção ao redor da área" markerLatitude={-24.4733834} markerLongitude={-46.2780295} markerHeight={60} markerWidth={60} circleWidth={200} circleHeight={200} markerUrl={"https://cdn-icons-png.flaticon.com/512/284/284483.png"}/>
+    </View>
+    )}
       </MapView>
       <MenuBar/>
     </View>
